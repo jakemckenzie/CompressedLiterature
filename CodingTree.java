f4655111e65cd29565446251456f45ab914d8092
jakemckenzie <jake314@uw.edu>
2018-04-21 22:11:03 -0700

    more skeleton

--- CodingTree.java
+++ CodingTree.java
@@ -2,17 +2,39 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
+import java.util.PriorityQueue;
 
 //cd C:\Users\Epimetheus\Documents\GitHub\CompressedLiterature
 //javac CodingTree.java
 public class CodingTree {
+    /**
+     * A constructor that takes the text of a message to be compressed. 
+     * The constructor is responsible for calling all private methods 
+     * that carry out the Huffman coding algorithm.
+     * @param message the message encoded by the huffman tree
+     */
+
     public CodingTree(String message) {
 
     }
+    /**
+     * @param codes A map of characters in the message with their binary codes.
+     */
 
     public Map<Character,String> codes;
 
+    /**
+     * @param bits A message encoded using Huffman codes.
+     */
+
     public List<Byte> bits;
+
+    /**
+     * ******************************EXTRA CREDIT**************************** 
+     * This method will take the output of Huffman’s encoding and produce the original text.
+     * @param bits A message encoded using Huffman codes.
+     * @param codes A map of characters in the message with their binary codes.
+     */
 
     public String decode(String bits, Map<Character,String> codes) {
         String temp = "";
