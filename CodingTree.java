d174ab6ba0f62297407c4beb75a39f506f9ac2fa
jakemckenzie <jake314@uw.edu>
2018-04-21 21:22:15 -0700

    coding tree skeleton

---
+++ CodingTree.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+//cd C:\Users\Epimetheus\Documents\GitHub\CompressedLiterature
+//javac CodingTree.java
+public class CodingTree {
+    public CodingTree(String message) {
+
+    }
+
+    public Map<Character,String> codes;
+
+    public List<Byte> bits;
+
+    public String decode(String bits, Map<Character,String> codes) {
+        String temp = "";
+
+        return temp;
+    }
+}
