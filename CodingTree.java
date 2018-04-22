99be7993b5533fe4cd7a33b3bf0162d3f0940423
jakemckenzie <jake314@uw.edu>
2018-04-22 16:20:02 -0700

    implemented huffman tree node

--- CodingTree.java
+++ CodingTree.java
@@ -25,12 +25,12 @@ public class CodingTree {
     /**
      * @param node the Huffman Tree node.
      */
-    public HuffmanNode node;
+    public Node node;
     /**
      * @param codes A map of characters in the message with their binary codes.
      */
 
-    public Map<Character,String> codes;
+    public HashMap<Character,String> codes;
 
     /**
      * @param bits A message encoded using Huffman codes.
@@ -55,7 +55,7 @@ public class CodingTree {
      * Builds a Huffman tree given some weights and an alphabet.
      * @param priQueue A priority queue of
      */
-    public void buildHuffmanTree(MyPriorityQueue<HuffmanNode> priQueue) {
+    public void buildHuffmanTree(MyPriorityQueue<Node> priQueue) {
 
     }
 
@@ -68,5 +68,65 @@ public class CodingTree {
         //for (char c: message.toCharArray()) 
         message.chars().forEachOrdered(c -> {if (count.containsKey((char)c)) count.put((char)c,count.get((char)c)+1);else count.put((char)c,1);});
         return count;
+    }
+    /**
+     * Huffman tree node.
+     */
+    public class Node implements Comparable<Node> {
+        /**
+        * @param key the char associated with the node.
+        */
+        public char key;
+        /**
+        * @param count the frequency of the character
+        */
+        public int count;
+        /**
+        * @param L the left node of the huffman tree.
+        */
+        public Node L;
+        /**
+        * @param R the right node of the huffman tree.
+        */
+        public Node R;
+        /**
+        * @param nodeChar key being sent into a node.
+        * @param nodeCount count being sent into a node.
+        */
+        public Node(char nodeChar, int nodeCount) {
+            key = nodeChar;
+            count = nodeCount;
+            L = null;
+            R = null;
+        }
+        /**
+        * @param left left node
+        * @param right right node
+        */
+        public Node(Node left, Node right) {
+            L = left;
+            R = right;
+            count = left.count + right.count;
+        }
+        /**
+        * Returns true if the receiver is a leaf.
+        */
+        private boolean isLeaf() {
+            assert ((L == null) && (R == null)||((L != null) && (R != null)));
+            return ((L == null) && (R == null));
+        }
+        /**
+        * Returns a string representation of the node.
+        */
+        public String toString() {
+            return (key + ": " + count);
+        }
+        /**
+        * Compares this node with the specified node for order.
+        */
+        @Override
+        public int compareTo(Node that){
+            return this.count - that.count;
+        }
     }
 }
--- MyPriorityQueue.java
+++ MyPriorityQueue.java
@@ -1,5 +1,6 @@
 import java.util.*;
 /**
+ * ******************************EXTRA CREDIT**************************** 
  * @author Jake McKenzie
  */
 
